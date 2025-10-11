ee373f49e6d1a2765d05816a7c68e3aea9103eec
icemao <jaimemao@outlook.com>
2025-10-11 09:46:40 +0000

    apply App changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import './App.css';
-import Register from './componets/Register';
+import "./App.css";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import HomePage from "./components/HomePage";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <Register/>
+    <Router>
+      <Routes>
+        <Route path="/register" element = {<Register/>}/>
+        <Route path="/login" element = {<Login/>}/>
+        <Route path="/" element = {<HomePage/>}/>
+      </Routes>
+    </Router>
   );
 }
 
