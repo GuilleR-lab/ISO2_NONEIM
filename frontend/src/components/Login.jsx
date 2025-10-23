2cbd33963ab5069d67ffe318e7a229587790241f
icemao <jaimemao@outlook.com>
2025-10-23 07:24:00 +0000

    add surname in register

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -5,6 +5,7 @@ const Login = () => {
     // Estados para los campos y mensajes
     const [phase, setPhase] = React.useState("login"); //states login, register
     const [username, setUsername] = React.useState("");
+    const [surname, setSurname] = React.useState("");
     const [address, setAddress] = React.useState("");
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
@@ -29,6 +30,19 @@ const Login = () => {
         if (!username) {
           inputUsernameRegister.style.borderColor = "red";
           inputUsernameRegister.style.background = "#FFF3F3";
+        }
+
+        const inputSurnameRegister = document.getElementById("surname");
+        inputSurnameRegister.addEventListener("input", function() {
+          if (this.value.length > 0) {
+            this.style.borderColor = "black";
+            this.style.background = "white";
+          }
+        });
+
+        if (!surname) {
+          inputSurnameRegister.style.borderColor = "red";
+          inputSurnameRegister.style.background = "#FFF3F3";
         }
 
         const inputAddressRegister = document.getElementById("address");
@@ -189,11 +203,10 @@ const Login = () => {
             <div className="form-container">
                 <form onSubmit={phase === "login" ? handleSubmitLogin : handleSubmitRegister}>  
                   
-                  {phase === "register" && 
-                  <button class="return_button" onClick={() => {
-                    setPhase(phase === "register" ? "login" : "register");
-                    setMessage("");
-                  }}>
+                  {phase === "register" && <button class="return_button" onClick={() => {
+                      setPhase(phase === "register" ? "login" : "register");
+                      setMessage("");
+                    }}>
                     <span>
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5"/>
@@ -228,6 +241,8 @@ const Login = () => {
                   { phase === "login" && <input type="password" id="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)}/> }
 
                   { phase === "register" && <input type="text" id="username" placeholder="Nombre de usuario" value={username} onChange={(e) => setUsername(e.target.value)}/> }
+
+                  { phase === "register" && <input type="text" id="surname" placeholder="Apellidos" value={surname} onChange={(e) => setSurname(e.target.value)}/> }
 
                   { phase === "register" && <input type="text" id="address" placeholder="Dirección" value={address} onChange={(e) => setAddress(e.target.value)}/> }
 
