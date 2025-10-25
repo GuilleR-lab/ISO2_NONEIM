c85fc2f5c239485bc0b1ef2811f898e22d6ebcf2
David Blanco <david.blanco5@alu.uclm.es>
2025-10-25 16:28:17 +0200

    Cambios en Auth (antiguo Login-Register) (references #14)
    Co-authored-by: Jaime Montalvo <jaimemao@outlook.com>

--- frontend/src/components/Auth.jsx
+++ frontend/src/components/Auth.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "../App.css";
+import { useNavigate } from 'react-router-dom';
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 
 const Auth = () => {
     // Estados para los campos y mensajes
-    const [phase, setPhase] = React.useState("login"); //states login, register
+    const [phase, setPhase] = React.useState("login"); //phases login, register
     const [username, setUsername] = React.useState("");
     const [surname, setSurname] = React.useState("");
     const [address, setAddress] = React.useState("");
@@ -11,13 +17,13 @@ const Auth = () => {
     const [password, setPassword] = React.useState("");
     const [message, setMessage] = React.useState("");
     const [showAuth, setShowAuth] = React.useState(true);
-    //2º estado Register
+    const [identifier, setIdentifier] = React.useState(""); // estado
+    
+    //2º Phase Register
     let handleSubmitRegister;
     if(phase === "register"){
       handleSubmitRegister = async (e) => {
         e.preventDefault();
-
-        // Validación rápida
         
         const inputUsernameRegister = document.getElementById("username");
         inputUsernameRegister.addEventListener("input", function() {
@@ -84,26 +90,32 @@ const Auth = () => {
           inputPasswordRegister.style.background = "#FFF3F3";
         }
 
-        if (!password || !username || !email || !address) {
+        if (!password || !username || !surname || !email || !address) {
           setMessage("⚠️ Es obligatorio rellenar todos los campos");
           return;
         }
         
         try {
-          const response = await fetch("http://localhost:8090/api/auth", {
+          const response = await fetch("http://localhost:8090/api/auth/register", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
             },
-            body: JSON.stringify({ username, address, email, password }),
+            body: JSON.stringify({ username, surname, address, email, password }),
           });
 
           const data = await response.json();
+          
+          setMessage(data.message);
 
-          setMessage(data.message || "✅ Usuario registrado correctamente");
-
-          if(data.message !== "✅ Usuario registrado correctamente"){
-            setPhase("login"); //cambiar estado
+          if(data.message !== "Usuario registrado correctamente"){
+            return;
+          }else{
+            const messageAuth = document.getElementById("auth-message");
+            messageAuth.style.color = "green";
+            await sleep(1000); //wait 1 second
+            navigate('/');
+            return;
           }
 
         } catch (error) {
@@ -113,11 +125,13 @@ const Auth = () => {
       };
     }
 
-    // Maneja el envío del formulario(login)
-    const handleSubmitLogin = async (e) => {
+    // Mannage the request by the form(login)
+  const navigate = useNavigate();
+
+  const handleSubmitLogin = async (e) => {
     e.preventDefault();
     
-    // cambia color cuando introducimos algo en el input
+    // change color when put anything in the input
     const inputUsername = document.getElementById("username");
     inputUsername.addEventListener("input", function() {
       if (this.value.length > 0) {
@@ -126,10 +140,14 @@ const Auth = () => {
       }
     });
 
-    if (!username) {
+    if(!username && !email){
       inputUsername.style.borderColor = "red";
       inputUsername.style.background = "#FFF3F3";
+    }
+
+    if ((!username && !email && password)) {
       setMessage("⚠️ Es obligatorio introducir el nombre o correo")
+      return;
     }
 
     const inputPassword = document.getElementById("password");
@@ -143,57 +161,65 @@ const Auth = () => {
     if (!password) {
       inputPassword.style.borderColor = "red";
       inputPassword.style.background = "#FFF3F3"
+    }
+    
+    if (!password && (username || email)) {
       setMessage("⚠️ Es obligatorio introducir la contraseña")
+      return;
     }
 
-
-
-    if (!username && !password) {
-      setMessage("⚠️ Es obligatorio rellenar todos los campos");
-      //    <span>
-      //      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-circle-fill" viewBox="0 0 16 16">
-      //        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4m.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2"/>
-      //      </svg>
-      //      <span>Es obligatorio rellenar todos los campos</span>
-      //    </span> 
-      
+    if ((!username && !email) && !password) {
+      setMessage("⚠️ Es obligatorio rellenar todos los campos"); 
       return;
     }
 
     try {
-      // Petición al backend (simulada o real si ya existe endpoint)
-      const isEmail = email === "" ? { username, password } : { email, password };
-      const response = await fetch("http://localhost:8090/api/auth", {
+      // Backend request
+      const isEmail = identifier.includes("@"); // o usar regex más estricto
+      const body = isEmail ? { email: identifier, password } : { username: identifier, password };
+      const response = await fetch("http://localhost:8090/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(isEmail),
+        body: JSON.stringify(body),
       });
-
-      if (!response.ok) {
-        throw new Error("Error en la autenticación o servidor no disponible");
-      }
 
       const data = await response.json();
 
-      setMessage(data.message || "✅ Inicio de sesión correcto");
+      setMessage(data.message);
 
-      if(data.message !== "✅ Inicio de sesión correcto"){
-        setPhase("register"); //cambiar estado
+      if(data.message !== "Inicio de sesion correcto"){
+        const messageAuth = document.getElementById("auth-message");
+        messageAuth.style.color = "red";
+        
+        await sleep(1000); //wait 1 second
+        setMessage("");
+        setPhase("register"); //change phase
+        
+        //clean data put in login phase
+        setEmail("");
+        setPassword("");
+        setUsername("");
+      } 
+      else {
+        const messageAuth = document.getElementById("auth-message");
+        messageAuth.style.color = "green";
+        
+        await sleep(1000); //wait 1 second
+        navigate('/');
+        return;
       }
 
     } catch (error) {
       console.error(error);
-      //setMessage("⚠️ Error al conectar con el servidor");
+      setMessage("⚠️ Error al conectar con el servidor");
     }
   };
 
     return (
         <>
-            
-
-            {/* Only for test */}
+            {/* Only for test(delete after test) */}
             <button class="button_change" id="button_id" onClick={() => {
               setPhase(phase === "register" ? "login" : "register");
               setMessage("");
@@ -205,6 +231,11 @@ const Auth = () => {
                   {phase === "register" && <button class="return_button" onClick={() => {
                       setPhase(phase === "register" ? "login" : "register");
                       setMessage("");
+
+                      //clean data put in register when back to login
+                      setPassword("");
+                      setUsername("");
+                      setEmail("");
                     }}>
                     <span>
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
@@ -223,25 +254,22 @@ const Auth = () => {
                       </svg>
                     </span>
                   </button>}
+
                   <h2>Iniciar sesión / Registrarse</h2>
                 </header>
 
                 <form onSubmit={phase === "login" ? handleSubmitLogin : handleSubmitRegister}>  
-                  
-                  
 
-                  { phase === "login" && <input type="text" id="username" placeholder="Usuario o correo" value={username} onChange={(e) => {
-                    const value = e.target.value;
-
-                    if(value.includes("@")){
-                      setEmail(value);
-                      setUsername("");
-                    }else{
-                      setUsername(value);
-                      setEmail("");
-                    }
-
-                  }}/> }
+                  {/*Ya se puede introducir un correo en el login, es decir que admita correo o usuario*/}
+                  { phase === "login" &&
+                    <input
+                      type="text"
+                      id="identifier"
+                      placeholder="Usuario o correo"
+                      value={identifier}
+                      onChange={(e) => setIdentifier(e.target.value)}
+                    />
+                  }
 
                   { phase === "login" && <input type="password" id="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)}/> }
 
@@ -251,14 +279,22 @@ const Auth = () => {
 
                   { phase === "register" && <input type="text" id="address" placeholder="Dirección" value={address} onChange={(e) => setAddress(e.target.value)}/> }
 
-                  { phase === "register" && <input type="text" id="email" placeholder="Correo electrónico" value={email} onChange={(e) => setEmail(e.target.value)}/> }
+                  { phase === "register" && <input type="email" id="email" placeholder="Correo electrónico" value={email} onChange={(e) => setEmail(e.target.value)}/> }
 
                   { phase === "register" && <input type="password" id="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)}/> }
                   
                   { phase === "register" && <input type="password" id="password" placeholder="Confirmar contraseña" value={password} onChange={(e) => setPassword(e.target.value)}/> }
                   
-                  {/* Mensaje de estado */}
-                  {message && <p class="login-message" >{message}</p>}
+                  {/* Phase message */}
+                  {message && (
+                    <div id="auth-message" className="login-message" role="alert" aria-live="polite">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                          className="bi bi-exclamation-circle-fill" viewBox="0 0 16 16" style={{marginRight:6}}>
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4m.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2"/>
+                      </svg>
+                      <span>{message}</span>
+                    </div>
+                  )}
 
                   <button style={{ backgroundColor: "#a1eafb" }}>
                     { phase === "login" ? "Iniciar sesión" : "Registrarme" }
