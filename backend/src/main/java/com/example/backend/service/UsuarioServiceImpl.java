118ad8c9371f37d7f047ec9a24dd4b6f80f83b47
GuilleR-lab <guillealde3@gmail.com>
2025-10-26 21:59:25 +0100

    Refactor UsuarioServiceImpl to enhance user checks

--- backend/src/main/java/com/example/backend/service/UsuarioServiceImpl.java
+++ backend/src/main/java/com/example/backend/service/UsuarioServiceImpl.java
@@ -4,19 +4,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.backend.model.Usuario;
 import com.example.backend.repository.UsuarioRepository;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
-
+    
     @Autowired
     private UsuarioRepository usuarioRepository;
 
     @Override
     public List<Usuario> showAllUsuarios() {
-        return usuarioRepository.findAll();
+        return (List<Usuario>) usuarioRepository.findAll(); 
     }
 
     @Override
@@ -26,21 +25,35 @@ public class UsuarioServiceImpl implements UsuarioService {
 
     @Override
     public boolean usuarioExists(String data) {
-        return usuarioRepository.existsByEmail(data);
+        return usuarioRepository.existsByEmail(data) || usuarioRepository.existsByUsername(data);
+    }
+
+    /*@Override
+        public Usuario getUsuarioById(Long id) {
+        return usuarioRepository.findById(id).orElse(null);
     }
 
     @Override
+    public Usuario findByEmail(String email){
+        return usuarioRepository.findByEmail(email);
+    }*/
+    //Update usuario
+    @Override
     public Usuario updateUsuario(Usuario newUsuario, Long usuarioId) {
         Usuario usuario = usuarioRepository.findById(usuarioId).orElse(null);
-
+        
         if (usuario == null) return null;
 
-        usuario.setNombre(newUsuario.getNombre());
+        usuario.setUsername(newUsuario.getUsername());
+        usuario.setSurname(newUsuario.getSurname());
+        usuario.setAddress(newUsuario.getAddress());
         usuario.setEmail(newUsuario.getEmail());
-        usuario.setContraseña(newUsuario.getContraseña());
+        usuario.setPassword(newUsuario.getPassword());
 
         return usuarioRepository.save(usuario);
     }
+
+
 
     @Override
     public void deleteUsuario(Long usuarioId) {
