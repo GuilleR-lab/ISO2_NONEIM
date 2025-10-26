3220716c2ef60280774899b4faf344b16a5e0f5a
GuilleR-lab <guillealde3@gmail.com>
2025-10-26 19:01:02 +0100

    Delete backend/src/main/java/com/example/backend/ReservaRepository.java

--- backend/src/main/java/com/example/backend/ReservaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.backend.model.Reserva;
-
-@Repository
-public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-}
