35f8b5383c87338d0b7714d04edafc688b191d7d
GuilleR-lab <guillealde3@gmail.com>
2025-10-26 20:24:12 +0100

    Add files via upload

---
+++ backend/src/main/java/com/example/backend/repository/ListaDeseosRepository.java
@@ -0,0 +1,7 @@
+package com.example.backend.repository;
+
+import com.example.backend.model.ListaDeseos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ListaDeseosRepository extends JpaRepository<ListaDeseos, Long> {
+}
