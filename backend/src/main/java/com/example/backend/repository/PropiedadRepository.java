04d24feff2211bac2294c2e613fa5c3a3d233374
David Blanco <david.blanco5@alu.uclm.es>
2025-10-26 23:59:28 +0100

    Elimianda la clase PropiedadRepository.java

--- backend/src/main/java/com/example/backend/repository/PropiedadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.backend.model.Propiedad;
-
-@Repository
-public interface PropiedadRepository extends JpaRepository<Propiedad, Long> {
-}
